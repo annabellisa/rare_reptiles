37631641b9edde39df9d09b854fdb4c9028b9815
annabellisa <annabel.smith@uq.edu.au>
2024-07-31 13:32:09 +1000

    revising data processing

--- 03_script/01_process_data.R
+++ 03_script/01_process_data.R
@@ -14,47 +14,6 @@ load("04_workspaces/processed_data.RData")
 # load packages
 library("lme4"); library("vegan")
 
-# Peer-review processing updates
-
-# R2 Comment 6 Line 363: There are no explicit models of dominance, such as the Berger-Parker index. Consider exploring Fisher’s alpha as an additional metric. 
-
-# R2 Comment 13 A further way to explore how patterns of commonness and rarity reflect patterns of richness would be to rank the community from most to least common, and vice versa, and iteratively assess how well the richness calculated via these two ordered rankings matches patterns of overall species richness. This approach is similar to the method used by Lennon et al. https://onlinelibrary.wiley.com/doi/abs/10.1046/j.1461-0248.2004.00548.x
-
-# -------- Berger-Parker  -------- #
-
-library(BiodiversityR)
-
-# R2 Comment 6 Line 363: There are no explicit models of dominance, such as the Berger-Parker index. Consider exploring Fisher’s alpha as an additional metric. 
-
-# Full data set (site x species matrix):
-# 14 sites, standardised for trap effort (captures / 1000 trap nights)
-head(sp_div2, 3); dim(sp_div2)
-
-# Rare species(site x species matrix):
-head(sp_div_25, 3); dim(sp_div_25)
-head(sp_div_5, 3); dim(sp_div_5)
-
-# Processed metrics with site data:
-head(sum_dat, 2);dim(sum_dat)
-
-# Caruso et al. 2007: 
-# Equation (2) was thus used to calculate the Berger–Parker index for each replicate sample with no further standardization
-# Eqn. 2:
-# K = NS / NT
-# where Ni is the abundance of the i-th species, NS indicates the abundance of the most abundant species, NT and S respectively are the total number of individuals and of species in a community sample
-  
-
-diversityresult(sp_div2,index="Berger", method="each site",sortit=F)
-
-fa.all<-fisher.alpha(sp_div2)
-fa.sp25<-fisher.alpha(sp_div_25)
-fa.sp5<-fisher.alpha(sp_div_5)
-
-# merge data:
-sum_dat <- merge(sum_dat, sr.rare, by="site", all.x = T, all.y = F)
-head(sum_dat, 6);dim(sum_dat)
-
-
 # -------- Pre-review data processing  -------- #
 
 # ----
@@ -72,143 +31,7 @@ head(sum_dat, 6);dim(sum_dat)
 
 # Following Rabinowitz (1981), we could look at rarity based on: geographic range, habitat specificity and local abundance. We've been talking about abundance-related rarity, but we could also look at geographical rarity - i.e. the proportion of sites in which each species occurs. Habitat specificity would have to be done through the literature. 
 
-# Data processing:
-# March 2020
-
-# Set up the data:
-
-dat1<-read.table("01_data/sp_summaries.txt",header=T)
-dat1$TSF2<-dat1$TSF^2
-dat1$TSFscale<-scale(dat1$TSF,center=T,scale=T)
-dat1$TSF2scale<-scale(dat1$TSF^2,center=T,scale=T)
-dat1$obs_effect<-as.numeric(1:nrow(dat1))
-dat1$effort<-as.numeric(dat1$effort)
-head(dat1); dim(dat1)
-
-# How many captures per location for each species?
-
-spnames<-names(dat1[,9:(length(dat1)-4)])
-names_lgth<-length(spnames)
-caps<-matrix(data=NA,nrow=length(spnames),ncol=2)
-
-for(i in 1:names_lgth){
-  
-  cols_thisrun<-colnames(dat1)==spnames[i]
-  caps[i,]<-tapply(dat1[,cols_thisrun],dat1$location,sum, na.rm=T)
-}
-caps<-data.frame(sp=spnames,caps)
-colnames(caps)<-c("sp",levels(dat1$location))
-caps
-
-
-# Set up a factor level which assigns each species to one of three groups: 1 = Hincks only, 2 = Pinks only, and 3 = both. The cut-off for analysis is >=28 Hincks and >=38 Pinks.
-
-sites_tab<-data.frame(Hsites=length(which(table(dat1$site, dat1$location)[,1]>0)),Psites=length(which(table(dat1$site, dat1$location)[,2]>0)))
-
-spnames2<-caps[,1]
-lgth2<-length(caps[,1])
-new.x<-matrix(data=NA,nrow=lgth2,ncol=1)
-
-for (i in 1:lgth2){
-  
-  new.x[i,]<- if (caps[i,2]>=28 & caps[i,3]<38) 1 
-  else if (caps[i,2]<28 & caps[i,3]>=38) 2
-  else 3
-}
-caps$mod_type<-as.factor(new.x)
-caps
-
-# STANDARDISE THE DATA BY TRAP EFFORT (dat4). 
-
-# This will turn the data into something like "the number of captures per 500 trap nights".
-
-head(dat1)
-lgth2<-length(caps[,1])
-lgth5<-length(dat1$obs)
-sp_cols<-dat1[,9:(length(dat1)-4)]
-div<-matrix(data=NA, nrow=lgth5, ncol=lgth2)
-
-for (i in 1:lgth2){
-  div[,i]<-(sp_cols[,i]/dat1$effort)*500
-  div[,i]<-round(div[,i],0)
-}
-div<-as.data.frame(div)
-colnames(div)<-caps$sp
-
-for(i in 1:lgth2){
-  div[,i]<-as.integer(div[,i])
-}
-dat4<-data.frame(dat1[,1:8],div,dat1[(length(dat1)-3):length(dat1)])
-head(dat1, 3)
-head(dat4, 3)
-
-compare<-data.frame(site=dat1$obs,raw=dat1$Pog,eff=dat1$effort,std=dat4$Pog)
-head(compare)
-
-# plot(compare$raw, compare$std)
-# cor.test(compare$raw, compare$std)
-
 # THE DATA:
-
-# mod_type can also be used to specify which data set to use:
-
-# 1 = Hdat (Hincks only, all sites)
-# 2 = Pdat (Pinks only, all sites)
-# 3 = dat1 (both locations, all sites)
-
-# 1:
-Hdat<-dat4[dat4$location=="Hincks",]
-Hdat<-droplevels(Hdat)
-rownames(Hdat)<-1:nrow(Hdat)
-Hdat$obs_effect<-as.numeric(1:nrow(Hdat))
-head(Hdat)
-
-# 2:
-Pdat<-dat4[dat4$location=="Pinkawillinie",]
-Pdat<-droplevels(Pdat)
-rownames(Pdat)<-1:nrow(Pdat)
-Pdat$obs_effect<-as.numeric(1:nrow(Pdat))
-head(Pdat)
-
-# 3: dat4
-dat4$obs_effect<-as.numeric(1:nrow(dat4))
-head(dat4)
-
-
-head(Hdat)
-levels(Hdat$site)
-
-newH<-Hdat[-which(Hdat$site %in% c("I1", "I2", "I3", "I4", "I7")),]
-newH<-droplevels(newH)
-levels(newH$site)
-head(newH); dim(newH)
-
-comb_dat<-as.data.frame(apply(newH[,c(which(colnames(newH)=="A_ina"):which(colnames(newH)=="D_dam"))],2,function(x) aggregate(x~newH[,which(colnames(newH)=="site")],FUN=sum)))
-comb_dat<-comb_dat[,grep(".x", colnames(comb_dat))]
-colnames(comb_dat)<-colnames(newH)[which(colnames(newH)=="A_ina"):which(colnames(newH)=="D_dam")]
-comb_dat<-comb_dat[,1:which(colnames(comb_dat)=="Pog")]
-comb_dat<-data.frame(site=unique(as.character(newH[,which(colnames(newH)=="site")])),comb_dat)
-
-# total abundance:
-comb_dat$abund<-rowSums(comb_dat[,2:ncol(comb_dat)])
-
-# species richness:
-comb_dat$sp_rich<-rowSums(t(apply(comb_dat[,c(which(colnames(comb_dat)=="A_ina"):which(colnames(comb_dat)=="Pog"))],1,function(x) ifelse(x==0,0,1)))) 
-head(comb_dat); dim(comb_dat)
-
-# relative abundance:
-rel_abund<-data.frame(site=comb_dat$site,round(apply(comb_dat[,c(which(colnames(comb_dat)=="A_ina"):which(colnames(comb_dat)=="Pog"))],2,function(x) x/comb_dat$abund),3), abund=comb_dat$abund, sp_rich=comb_dat$sp_rich)
-
-# simpson's index:
-rel_abund$simps_ind<-1-(rowSums(rel_abund[,c(which(colnames(rel_abund)=="A_ina"):which(colnames(rel_abund)=="Pog"))]^2))
-
-# reciprocal simpson's index:
-rel_abund$simps_recip<-1/rel_abund$simps_ind
-head(rel_abund); dim(rel_abund)
-
-diversity(rel_abund[,c(which(colnames(rel_abund)=="A_ina"):which(colnames(rel_abund)=="Pog"))], index="simpson")
-
-# barplot(rel_abund$simps_ind[c(3:6,1:2)])
 
 # full data set:
 
@@ -304,7 +127,7 @@ sum_dat$fire_cat <- factor(sum_dat$fire_cat, levels = c("Unburnt", "Medium", "Bu
 # making site a factor
 sum_dat$site <- as.factor(sum_dat$site)
 
-#List of 14 sites, taking site column and calling "H" to create 2 different variables for site, either "H" or "P"
+# List of 14 sites, taking site column and calling "H" to create 2 different variables for site, either "H" or "P"
 sum_dat$location <- NA
 sum_dat$location[which(unlist(gregexpr("H", sum_dat$site)) == 1)] <- "H"
 sum_dat$location[which(unlist(gregexpr("H", sum_dat$site)) == -1)] <- "P"
@@ -343,19 +166,21 @@ length(sp_25) # 14 rare species in the lowest 25% category
 sp_5 <- sp_sum2$Species_Code[which(sp_sum2$max_5 == "r")]
 length(sp_5) # 25 rare species in the max. 5% cateogry
 
-#pulling out data set for rare 25% species
+# standardised abundance data for rare 25% species
 sp_div_25 <- sp_div2[,which(colnames(sp_div2)%in%sp_25)]
 head(sp_div_25, 3); dim(sp_div_25)
 
-#pulling out data set for rare 5% species
+# standardised abundance data set for rare 5% species
 sp_div_5 <- sp_div2[,which(colnames(sp_div2)%in%sp_5)]
 head(sp_div_5, 3); dim(sp_div_5)
 
-rare.data <- data.frame(site=names(rowSums(sp_div_25)), abund_25 = rowSums(sp_div_25), pres_25 = ifelse(rowSums(sp_div_25) == 0, 0, 1), abund_5 = rowSums(sp_div_5), pres_5 = ifelse(rowSums(sp_div_5) == 0, 0, 1))
+rare.data <- data.frame(site=names(rowSums(sp_div_25)), rel_abund_25 = rowSums(sp_div_25), pres_25 = ifelse(rowSums(sp_div_25) == 0, 0, 1), rel_abund_5 = rowSums(sp_div_5), pres_5 = ifelse(rowSums(sp_div_5) == 0, 0, 1))
 
 row.names(rare.data) <- 1:nrow(rare.data)
 head(sum_dat, 6);dim(sum_dat)
 head(rare.data); dim(rare.data)
+
+### UP TO HERE, NEED TO RE-DO THE SPECIES RICHNESS OF RARE SPECIES, BEFORE GOING TO BERGER PARKER
 
 # What about species richness of rare species?
 
@@ -375,55 +200,75 @@ sum_dat <- merge(sum_dat, rare.data, by="site", all.x = T, all.y = F)
 sum_dat <- merge(sum_dat, sr.rare, by="site", all.x = T, all.y = F)
 head(sum_dat, 6);dim(sum_dat)
 
-# calculating Shannon's, Evenness and Simpsons index for rare species
-
-head(sp_div_25, 3); dim(sp_div_25)
-head(sp_div_5, 3); dim(sp_div_5)
-
-# rare 25% species
-
-# simpson's index
-sum_dat$inv_simps_ind25<-1/rowSums(sp_div_25^2)
-# replace 'Inf' with 0s
-sum_dat$inv_simps_ind25[which(sum_dat$inv_simps_ind25 == 'Inf')]<- 0
-
-# evenness, from Simpson's
-sum_dat$even25<-sum_dat$inv_simps_ind25/sum_dat$sr_25
-sum_dat$even25[which(sum_dat$even25 == 'NaN')]<- 0
-
-# shannon's index:
-log_relab25<-log(sp_div_25)
-log_relab25<-t(apply(log_relab25,1,function(x) ifelse(is.infinite(x)==T,0,x)))
-
-sum_dat$shann_ind25<--rowSums(sp_div_25*log_relab25)
-
-# evenness, from Shannon's:
-sum_dat$Hmax_25<-log(sum_dat$sr_25)
-sum_dat$even2_25<-sum_dat$shann_ind25/sum_dat$Hmax_25
-
-#rare 5% species
-
-# simpson's index
-sum_dat$inv_simps_ind5<-1/rowSums(sp_div_5^2)
-
-# evenness, from Simpson's:
-sum_dat$even5<-sum_dat$inv_simps_ind5/sum_dat$sr_5
-
-# shannon's index:
-log_relab5<-log(sp_div_5)
-log_relab5<-t(apply(log_relab5,1,function(x) ifelse(is.infinite(x)==T,0,x)))
-
-sum_dat$shann_ind5<--rowSums(sp_div_5*log_relab5)
-
-# evenness, from Shannon's:
-sum_dat$Hmax_5<-log(sum_dat$sr_5)
-sum_dat$even2_5<-sum_dat$shann_ind5/sum_dat$Hmax_5
+# From the MS: "For the community of rare species, we did not model diversity metrics that required proportional abundances (Simpson’s Diversity, Shannon’s Diversity, Berger-Parker Dominance) because the abundance data were dominated by zeros and ones."
 
 dir()
 # save.image("04_workspaces/processed_data.RData")
 
 # ----
 
+# Peer-review processing updates
+
+# R2 Comment 6 Line 363: There are no explicit models of dominance, such as the Berger-Parker index. Consider exploring Fisher’s alpha as an additional metric. 
+
+# R2 Comment 13 A further way to explore how patterns of commonness and rarity reflect patterns of richness would be to rank the community from most to least common, and vice versa, and iteratively assess how well the richness calculated via these two ordered rankings matches patterns of overall species richness. This approach is similar to the method used by Lennon et al. https://onlinelibrary.wiley.com/doi/abs/10.1046/j.1461-0248.2004.00548.x
+
+# -------- Berger-Parker  -------- #
+
+# R2 Comment 6 Line 363: There are no explicit models of dominance, such as the Berger-Parker index. Consider exploring Fisher’s alpha as an additional metric. 
+
+# Caruso et al. 2007: 
+# "Equation (2) was thus used to calculate the Berger–Parker index for each replicate sample with no further standardization
+# Eqn. 2:
+# K = NS / NT
+# where Ni is the abundance of the i-th species, NS indicates the abundance of the most abundant species, NT and S respectively are the total number of individuals and of species in a community sample..."
+
+# Berger & Parker 1970"
+# "...species dominance is given as:
+# Dd = p max
+
+# where p max is the maximum proportion of any one species in a sample..."
+
+# Kitikidou et al. 2024:
+# "The Berger–Parker index provides a simple quantification of the most abundant species in a given sample. It ranges from 0 to 1, with 0 indicating complete evenness or equal abundance among all species in the community. Conversely, a value of 1 indicates complete dominance, with one species accounting for all individuals in the community. 
+# The Berger–Parker index is a simple and straightforward measure that can be easily calculated, making it useful for preliminary analyses or comparisons of dominance across different samples. However, it is important to note that the Berger–Parker index only considers the dominance of a single species and does not provide information about the overall diversity or richness of the community..."
+"BP = max(pi)"
+# The closer BP is to 0, the greater the diversity."
+
+# It's the maximum proportional abundance
 
 
+# Full data set (site x species matrix):
+# 14 sites, standardised for trap effort (captures / 1000 trap nights)
+head(sp_div2, 3); dim(sp_div2)
+
+# Rare species(site x species matrix):
+head(sp_div_25, 3); dim(sp_div_25)
+head(sp_div_5, 3); dim(sp_div_5)
+
+# Processed metrics with site data:
+head(sum_dat, 2);dim(sum_dat)
+
+
+# merge data:
+sum_dat <- merge(sum_dat, sr.rare, by="site", all.x = T, all.y = F)
+head(sum_dat, 6);dim(sum_dat)
+
+# Relative abundance already calculated
+head(rel_abund2,3); dim(rel_abund2)
+
+bergpark<-data.frame(site=names(apply(rel_abund2,1,function(x) max(x))),bp_ind=apply(rel_abund2,1,function(x) max(x)))
+
+bergpark$site==sum_dat$site
+
+plot(bergpark$bp_ind, sum_dat$shann_ind)
+plot(sum_dat$fire_cat, bergpark$bp_ind)
+
+
+
+# Fisher's alpha:
+
+fa.all<-fisher.alpha(sp_div2)
+fa.sp25<-fisher.alpha(sp_div_25)
+fa.sp5<-fisher.alpha(sp_div_5)
 
